3f9536eab55a0ac0a1f5e7c713943cf4d7b7ffcf
Praneetha167 <154442872+Praneetha167@users.noreply.github.com>
2024-09-24 22:08:24 +0530

    Add files via upload

---
+++ declare_class_and_object.java
@@ -0,0 +1,11 @@
+package src;
+
+public class declare_class_and_object {
+	int x=10;
+	public static void main (String[]args) {
+		declare_class_and_object obj=new declare_class_and_object();
+		System.out.println(obj.x);
+		
+	}
+
+}
