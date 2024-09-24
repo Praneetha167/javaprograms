f3471c5d362ce66cc4c7cda89d2c00c0aaa586f4
Praneetha167 <154442872+Praneetha167@users.noreply.github.com>
2024-09-24 22:14:57 +0530

    Add files via upload

---
+++ multiple_objects.java
@@ -0,0 +1,15 @@
+package src;
+
+public class multiple_objects {
+	int x=45;
+	String s="Happy";
+	public static void main(String[] args) {
+		multiple_objects intobj=new multiple_objects();
+		multiple_objects stringobj=new multiple_objects();
+		System.out.println(intobj.x);
+		System.out.println(stringobj.s);
+		// TODO Auto-generated method stub
+
+	}
+
+}
