8ead723d432d2836e113edbada4b5657cefdddff
Praneetha167 <154442872+Praneetha167@users.noreply.github.com>
2024-09-25 20:11:38 +0530

    Add files via upload

---
+++ constructor.java
@@ -0,0 +1,17 @@
+package src;
+
+public class constructor {
+	int x;
+	public constructor()
+	{
+		x=46;
+	}
+
+	public static void main(String[] args) {
+		constructor obj=new constructor();
+		System.out.println("The value is:"+obj.x);
+		// TODO Auto-generated method stub
+
+	}
+
+}
