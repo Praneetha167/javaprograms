c48a2d4c48e88d169651dcf2bdb293550ed752c6
Praneetha167 <154442872+Praneetha167@users.noreply.github.com>
2024-09-25 20:08:20 +0530

    Add files via upload

---
+++ add_parameters.java
@@ -0,0 +1,15 @@
+package src;
+
+public class add_parameters {
+	public int add(int x,int y)
+	{
+		return (x+y);
+	}
+	public static void main(String[] args) {
+		add_parameters obj=new add_parameters();
+		System.out.println("the sum is" +obj.add(10, 8));
+		// TODO Auto-generated method stub
+
+	}
+
+}
