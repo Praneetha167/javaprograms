de757db3170e790d4e249278e55e993f65c6ab87
Praneetha167 <154442872+Praneetha167@users.noreply.github.com>
2024-10-03 11:55:13 +0530

    Add files via upload

---
+++ firstframe.java
@@ -0,0 +1,13 @@
+package awt;
+import java.awt.Frame;
+public class firstframe {
+
+	public static void main(String[] args) {
+		Frame f=new Frame("My first frame");
+		f.setSize(400,200);
+		f.setVisible(true);
+		// TODO Auto-generated method stub
+
+	}
+
+}
